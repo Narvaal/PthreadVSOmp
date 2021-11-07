52d04a408625a68ffa3c19cf76b98a0d1f6d10b1
Alessandro Bezerra <44413008+Narvaal@users.noreply.github.com>
2021-11-07 17:44:30 -0300

    Update TestePthread.c
    Timer adicionado

--- TestePthread.c
+++ TestePthread.c
@@ -5,6 +5,9 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <pthread.h>
+#include <time.h>
+
+#define BILLION 1000000000.0 //timer config
 
 #define lenght 1000000000 //Tamanho do vetor a ser executado
 int *vector;
@@ -20,7 +23,6 @@ void *myThreadFun(void *vargp);
 /*
   Main onde é criado as 4 threads 
 */
-
 int main(int argc, char* argv[])
 {
     //Vetor --------------------------
@@ -34,7 +36,14 @@ int main(int argc, char* argv[])
     }
     //Vetor --------------------------
 
-    printf("Inicioando o programa...\n");
+    printf("Iniciando o programa...\n");
+  
+
+    //               <timer>
+    struct timespec start, end;
+    clock_gettime(CLOCK_REALTIME, &start);
+    //               <timer>
+
     //Inicialização....
     sizeThread = atoi(argv[1]);
     int * arg = malloc(sizeof(int) * sizeThread);
@@ -47,17 +56,22 @@ int main(int argc, char* argv[])
     for(int k = 0 ;k < sizeThread ;k++){
         pthread_join(threads[k], NULL);
     }
+
+    //               <fim-timer>
+    clock_gettime(CLOCK_REALTIME, &end);
+    double time_spent = (end.tv_sec - start.tv_sec) +
+                      (end.tv_nsec - start.tv_nsec) / BILLION;
+
+    printf("Tempo de execução %f \n", time_spent);
+    //               <fim-timer>
+
     free(threads);
-
-
     printf("Finalizado...\n");
-
     /*
     for(int x = 0 ;x < lenght ;x++){
         printf("size -> %d \n",vector[x]);
     }
     */
-
     free(vector);
     return 0;
 }
